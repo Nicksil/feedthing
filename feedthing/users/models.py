63da1d81cd7be665fb964498e66581f4920dc58d
Nick Silvester <nicksil@gmail.com>
2017-04-17 01:01:12 -0400

    User admin BS

--- feedthing/users/models.py
+++ feedthing/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.base_user import BaseUserManager
-from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 from core.models import TimeStampedModel
@@ -24,19 +23,24 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_admin', True)
+        extra_fields.setdefault('is_staff', True)
 
         if extra_fields.get('is_admin') is False:
             raise ValueError('Superuser must have is_admin=True.')
 
+        if extra_fields.get('is_staff') is False:
+            raise ValueError('Superuser must have is_staff=True.')
+
         return self.create_user(email, password, **extra_fields)
 
 
-class User(AbstractBaseUser, PermissionsMixin, TimeStampedModel):
+class User(AbstractBaseUser, TimeStampedModel):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=255, blank=True)
     last_name = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserManager()
 
