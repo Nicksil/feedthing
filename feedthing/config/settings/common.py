6015d070edc3ed82ade0c392134cdbcbc56c5f1b
Nick Silvester <nicksil@gmail.com>
2017-04-17 00:31:51 -0400

    Install apps

--- feedthing/config/settings/common.py
+++ feedthing/config/settings/common.py
@@ -32,7 +32,10 @@ DJANGO_APPS = (
 THIRD_PARTY_APPS = (
     'django_extensions',
 )
-LOCAL_APPS = ()
+LOCAL_APPS = (
+    'core',
+    'users',
+)
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
