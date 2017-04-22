c1fe31253091b060eb849ff89c1ad64e6f7f3769
Nick Silvester <nicksil@gmail.com>
2017-04-22 18:29:16 -0400

    Figure out struct_time and create helper method to convert those objects into datetime.datetime objects for DB consumption

--- feedthing/api/views.py
+++ feedthing/api/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, status
+from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
 
 from .serializers import FeedSerializer
+from feeds.managers import FeedEntryManager
 from feeds.models import Feed
 
 
@@ -18,4 +20,10 @@ class FeedViewSet(viewsets.ModelViewSet):
     @detail_route(methods=['post', 'put'])
     def fetch(self, request, pk=None):
         obj = self.get_object()
+        result = FeedEntryManager.fetch_and_save(obj)
+        # ------------- PDEBUG -------------
+        print('=' * 100)
+        print(result)
+        print('=' * 100)
+        # ------------- PDEBUG -------------
         return Response(status=status.HTTP_200_OK)
--- feedthing/feeds/managers.py
+++ feedthing/feeds/managers.py
@@ -1,15 +1,48 @@
+import datetime
+import time
+
 import feedparser
 
-
-def fetch_entries(url):
-    parsed = feedparser.parse(url)
-    return parsed.get('entries', [])
+from feeds.models import Entry
 
 
-def prepare_entry(entry, feed):
-    return {
-        'feed': feed,
-        'link': entry.get('link', ''),
-        'published': entry.get('published_parsed', None),
-        'title': entry.get('title', ''),
-    }
+class FeedEntryManager:
+    def __init__(self, feed):
+        self.feed = feed
+
+    def fetch(self):
+        parsed = feedparser.parse(self.feed.href)
+        return parsed.get('entries', [])
+
+    def prepare(self, entry):
+        _published = entry.get('published_parsed', None)
+
+        if _published and isinstance(_published, time.struct_time):
+            _published = self.convert_struct_time(_published)
+
+        return {
+            'feed': self.feed,
+            'link': entry.get('link', ''),
+            'published': _published,
+            'title': entry.get('title', ''),
+        }
+
+    def convert_struct_time(self, value):
+        """Converts a time.struct_time object to a datetime.datetime object"""
+        return datetime.datetime(time.mktime(value))
+
+    @staticmethod
+    def save(data):
+        return Entry.objects.create(**data)
+
+    @classmethod
+    def fetch_and_save(cls, feed):
+        _instance = cls(feed)
+        entries = _instance.fetch()
+        parsed = [_instance.prepare(e) for e in entries]
+        # ------------- PDEBUG -------------
+        print('=' * 100)
+        print(parsed)
+        print('=' * 100)
+        # ------------- PDEBUG -------------
+        return [_instance.save(p) for p in parsed]
