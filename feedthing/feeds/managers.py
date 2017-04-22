e92d9d19a92a13783f5799aac189c87abe1c2a78
Nick Silvester <nicksil@gmail.com>
2017-04-22 17:57:14 -0400

    Remove feeds.views

--- feedthing/feeds/managers.py
+++ feedthing/feeds/managers.py
@@ -4,3 +4,12 @@ import feedparser
 def fetch_entries(url):
     parsed = feedparser.parse(url)
     return parsed.get('entries', [])
+
+
+def prepare_entry(entry, feed):
+    return {
+        'feed': feed,
+        'link': entry.get('link', ''),
+        'published': entry.get('published_parsed', None),
+        'title': entry.get('title', ''),
+    }
