fda3a953b8c2c6932e10ef4adda67e7bb0f3aa30
Nick Silvester <nicksil@gmail.com>
2017-04-22 20:26:09 -0400

    Remove authentication/de-authentication functions. Unneeded for time-being.

--- feedthing/users/urls.py
+++ feedthing/users/urls.py
@@ -1,10 +1,1 @@
-from django.conf.urls import url
-
-from . import views
-
-
-urlpatterns = [
-    url(r'^authenticate/$', views.authenticate, name='authenticate'),
-    url(r'^create/$', views.register, name='register'),
-    url(r'^sign-out/$', views.sign_out, name='sign_out'),
-]
+urlpatterns = []
