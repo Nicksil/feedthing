c58fc29a5b2e688f7a8b4ca09abb66669e2aa1d1
Nick Silvester <nicksil@gmail.com>
2017-04-30 19:50:16 -0400

    ...

--- feedthing/feeds/api/views.py
+++ feedthing/feeds/api/views.py
@@ -1,14 +1,15 @@
+import datetime
 import time
 
-import datetime
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.response import Response
 import feedparser
 
-from core.utils import ensure_aware
 from ..models import Feed
 from .serializers import FeedHyperlinkedModelSerializer
+from core.utils import ensure_aware
+from entries.api.serializers import EntryHyperlinkedModelSerializer
 
 
 class FeedAPIViewSet(viewsets.ModelViewSet):
@@ -18,45 +19,62 @@ class FeedAPIViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         feed_data = feedparser.parse(request.data['href'])
 
-        etag = feed_data.get('etag', '')
-        href = feed_data.get('href', '')
+        parsed = self.parse_feed_response(feed_data)
+        _entries = parsed.pop('entries')
+        request.data.update(parsed)
 
-        _entries = feed_data.get('entries', [])
-        _feed = feed_data.get('feed')
-        _headers = feed_data.get('headers')
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        feed = serializer.save()
+        feed.users.add(request.user)
 
-        if _headers and 'Last-Modified' in _headers:
-            last_modified = _headers['Last-Modified']
-        else:
-            last_modified = ''
+        for _entry in _entries:
+            entry_parsed = self.parse_entry_response(_entry)
+            entry_parsed.update({'feed': serializer.data['url']})
 
+            _entry_serializer = EntryHyperlinkedModelSerializer(data=entry_parsed)
+            _entry_serializer.is_valid(raise_exception=True)
+            _entry_serializer.save()
+
+        headers = self.get_success_headers(serializer.data)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    @staticmethod
+    def parse_feed_response(response: feedparser.FeedParserDict) -> dict:
+        _feed = response.get('feed')
         if _feed and 'title' in _feed:
             title = _feed['title']
         else:
             title = ''
 
-        request.data.update({
-            'etag': etag,
-            'href': href,
+        _headers = response.get('headers')
+        if _headers and 'Last-Modified' in _headers:
+            last_modified = _headers['Last-Modified']
+        else:
+            last_modified = ''
+
+        return {
+            'entries': response.get('entries', []),
+            'etag': response.get('etag', ''),
+            'href': response.get('href', ''),
             'last_modified': last_modified,
             'title': title,
-        })
+        }
 
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+    @staticmethod
+    def parse_entry_response(entry: feedparser.FeedParserDict) -> dict:
+        if 'feedburner_origlink' in entry:
+            link = entry['feedburner_origlink']
+        else:
+            link = entry.get('link', '')
 
-        for _entry in _entries:
-            if 'feedburner_origlink' in _entry:
-                link = _entry['feedburner_origlink']
-            else:
-                link = _entry.get('link', '')
+        _published = entry.get('published_parsed', None)
+        if _published and isinstance(_published, time.struct_time):
+            _published = ensure_aware(datetime.datetime.fromtimestamp(time.mktime(_published)))
 
-            _published = _entry.get('published_parsed', None)
-            if _published and isinstance(_published, time.struct_time):
-                _published = ensure_aware(datetime.datetime.fromtimestamp(time.mktime(_published)))
-
-            title = _entry.get('title', '')
-
-        headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return {
+            'href': link,
+            'published': _published,
+            'title': entry.get('title', ''),
+        }
