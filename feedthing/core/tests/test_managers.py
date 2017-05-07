88e2979804fffecd10a598807efbb6da53e992cf
Nick Silvester <nicksil@gmail.com>
2017-05-07 18:35:54 -0400

    More core.managers tests

--- feedthing/core/tests/test_managers.py
+++ feedthing/core/tests/test_managers.py
@@ -9,7 +9,7 @@ from django.test import TestCase
 import feedparser
 
 from feeds.tests.factories import FeedFactory
-from ..managers import FeedDataManager
+from ..managers import FeedDataManager, EntryDataManager
 
 
 def get_feedparser_parsed():
@@ -41,6 +41,14 @@ class CoreManagersTestCase(TestCase):
         mgr = FeedDataManager(href='https://example.com')
         data = mgr.fetch_data()
         result = mgr.to_internal(data)
+
+        self.assertIsInstance(result, dict)
+
+    @mock.patch.object(feedparser, 'parse')
+    def test_FeedDataManager_fetch_classmethod_returns_dict(self, parse):
+        parse.return_value = self.feedparserdict
+
+        result = FeedDataManager.fetch(href='https://example.com')
 
         self.assertIsInstance(result, dict)
 
@@ -84,3 +92,68 @@ class CoreManagersTestCase(TestCase):
         mgr = FeedDataManager(feed=fake_feed, href=fake_href)
 
         self.assertEqual(mgr._href, fake_href)
+
+    @mock.patch.object(feedparser, 'parse')
+    def test_FeedDataManager_entries_property_call_returns_list(self, parse):
+        parse.return_value = self.feedparserdict
+
+        mgr = FeedDataManager(href='https://example.com')
+        mgr.fetch_data()
+        result = mgr.entries
+
+        self.assertIsInstance(result, list)
+
+    def test_FeedDataManager_entries_property_call_returns_empty_list(self):
+        mgr = FeedDataManager()
+        result = mgr.entries
+
+        self.assertEqual(len(result), 0)
+
+    def test_EntryDataManager_to_internal_returns_dict(self):
+        feed_data = get_feedparser_parsed()
+        mgr = EntryDataManager(feed_data.entries[0])
+        result = mgr.to_internal()
+
+        self.assertIsInstance(result, dict)
+
+    def test_EntryDataManager_get_href_returns_str(self):
+        feed_data = get_feedparser_parsed()
+        mgr = EntryDataManager(feed_data.entries[0])
+        result = mgr._get_href()
+
+        self.assertIsInstance(result, str)
+
+    def test_EntryDataManager_get_href_without_feedburner_link_returns_str(self):
+        feed_data = get_feedparser_parsed()
+        entry = feed_data.entries[0]
+        entry.pop('feedburner_origlink', None)
+        mgr = EntryDataManager(entry)
+        result = mgr._get_href()
+
+        self.assertIsInstance(result, str)
+
+    def test_EntryDataManager_get_published_returns_datetime_object(self):
+        feed_data = get_feedparser_parsed()
+        mgr = EntryDataManager(feed_data.entries[0])
+        result = mgr._get_published()
+
+        self.assertIsInstance(result, datetime.datetime)
+
+    def test_EntryDataManager_get_published_returns_None(self):
+        feed_data = get_feedparser_parsed()
+        entry = feed_data.entries[0]
+        entry.pop('published_parsed', None)
+        mgr = EntryDataManager(entry)
+        result = mgr._get_published()
+
+        self.assertIsNone(result)
+
+    @mock.patch.object(feedparser, 'parse')
+    def test_EntryDataManager_parse_classmethod_raise_TypeError_when_called_with_many_is_True_and_passing_in_single_item(self, parse):
+        parse.return_value = self.feedparserdict
+
+        feed_mgr = FeedDataManager(href='https://example.com')
+        feed_mgr.fetch_data()
+
+        with self.assertRaises(TypeError):
+            EntryDataManager.parse(feed_mgr.entries[0], many=True)
