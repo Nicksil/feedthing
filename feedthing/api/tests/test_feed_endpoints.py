b8cb61bed1e1a37909ad120e3406ced768fc584c
Nick Silvester <nicksil@gmail.com>
2017-05-06 22:51:45 -0400

    Add testing settings file

--- feedthing/api/tests/test_feed_endpoints.py
+++ feedthing/api/tests/test_feed_endpoints.py
@@ -1,23 +1,21 @@
+from django.test import TestCase
 from django.urls import reverse
-
-from rest_framework import status
-from rest_framework.test import APITestCase
 
 from users.tests.factories import UserFactory
 
 
-class FeedEndpointsTests(APITestCase):
+class FeedEndpointsTests(TestCase):
     def setUp(self):
         self.simple_user_pw = 'simple_password'
         self.simple_user = UserFactory(password=self.simple_user_pw)
 
     def test_feed_index_GET_request_returns_status_200(self):
         self.client.login(
-            username=self.simple_user.email,
+            email=self.simple_user.email,
             password=self.simple_user_pw,
         )
 
         url = reverse('feedthing-api-v1-feed-index')
         response = self.client.get(url)
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, 200)
---
+++ feedthing/config/settings/testing.py
@@ -0,0 +1,22 @@
+from .common import *
+
+DEBUG = False
+EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+PASSWORD_HASHERS = ('django.contrib.auth.hashers.MD5PasswordHasher',)
+REST_FRAMEWORK['DEFAULT_AUTHENTICATION_CLASSES'] = ('rest_framework.authentication.SessionAuthentication',)
+TEMPLATES[0]['OPTIONS']['debug'] = False
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': '',
+    }
+}
+
+TEMPLATES[0]['OPTIONS']['loaders'] = [(
+    'django.template.loaders.cached.Loader',
+    [
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    ],
+)]
