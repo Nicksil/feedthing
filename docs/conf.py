381c3b901f01f1c5f08edf0650c6c0844acfb063
Nick Silvester <nicksil@gmail.com>
2017-05-07 20:12:31 -0400

    Starting docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+import time
 
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
@@ -31,12 +32,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -52,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'feedthing'
-copyright = '2017, Nicholas Silvester'
+copyright = '2017-{}, Nicholas Silvester'.format(time.strftime('%Y'))
 author = 'Nicholas Silvester'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -82,7 +84,6 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -94,19 +95,23 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {
+    'github_banner': True,
+    'github_repo': 'feedthing',
+    'github_user': 'Nicksil',
+    'show_powered_by': False,
+    'show_related': False,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'feedthingdoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -136,7 +141,6 @@ latex_documents = [
      'Nicholas Silvester', 'manual'),
 ]
 
-
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
@@ -145,7 +149,6 @@ man_pages = [
     (master_doc, 'feedthing', 'feedthing Documentation',
      [author], 1)
 ]
-
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -158,8 +161,9 @@ texinfo_documents = [
      'Miscellaneous'),
 ]
 
-
-
-
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+html_show_sourcelink = False
+html_show_sphinx = False
+html_show_copyright = True
--- docs/index.rst
+++ docs/index.rst
@@ -3,18 +3,20 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to feedthing's documentation!
-=====================================
+Feedthing: Because
+==================
+
+Release v\ |version|.
+
+.. image:: https://travis-ci.org/Nicksil/feedthing.svg?branch=master
+   :target: https://travis-ci.org/Nicksil/feedthing
+
+.. image:: https://codecov.io/gh/Nicksil/feedthing/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/Nicksil/feedthing
+
+**Feedthing** was born out of a desire to be able to browse my feeds without
+the extra cruft of "*recommendations*," "*likes*," "*follow*," "*curated*," etc.
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-
-
-
-Indices and tables
-==================
-
-* :ref:`genindex`
-* :ref:`modindex`
-* :ref:`search`
