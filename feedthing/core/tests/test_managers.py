3a62d793e8d1fa8f4d4f4eb4e67bbc11c4a10ba2
Nick Silvester <nicksil@gmail.com>
2017-05-17 20:53:17 -0400

    Remove tests built for replaced FeedManager API

--- feedthing/core/tests/test_managers.py
+++ feedthing/core/tests/test_managers.py
@@ -1,7 +1,5 @@
-from unittest import mock
 import datetime
 import os
-import time
 
 from django.conf import settings
 from django.test import TestCase
@@ -9,8 +7,6 @@ from django.test import TestCase
 import feedparser
 
 from ..managers import EntryDataManager
-from ..managers import FeedManager
-from feeds.tests.factories import FeedFactory
 
 
 def get_feedparser_parsed():
@@ -25,90 +21,6 @@ def get_feedparser_parsed():
 class CoreManagersTestCase(TestCase):
     def setUp(self):
         self.feedparserdict = get_feedparser_parsed()
-
-    @mock.patch.object(feedparser, 'parse')
-    def test_FeedDataManager_fetch_data_returns_FeedParserDict(self, parse):
-        parse.return_value = self.feedparserdict
-
-        mgr = FeedManager(href='https://example.com')
-        result = mgr.fetch_data()
-
-        self.assertIsInstance(result, feedparser.FeedParserDict)
-
-    @mock.patch.object(feedparser, 'parse')
-    def test_FeedDataManager_to_internal_returns_dict(self, parse):
-        parse.return_value = self.feedparserdict
-
-        mgr = FeedManager(href='https://example.com')
-        data = mgr.fetch_data()
-        result = mgr.to_dict(data)
-
-        self.assertIsInstance(result, dict)
-
-    @mock.patch.object(feedparser, 'parse')
-    def test_FeedDataManager_fetch_classmethod_returns_dict(self, parse):
-        parse.return_value = self.feedparserdict
-
-        result = FeedManager.fetch(href='https://example.com')
-
-        self.assertIsInstance(result, dict)
-
-    def test_FeedDataManager_get_last_modified_returns_datetime_object(self):
-        fake = feedparser.FeedParserDict()
-        fake['modified_parsed'] = time.localtime()
-
-        mgr = FeedManager()
-        result = mgr._get_last_modified(fake)
-
-        self.assertIsInstance(result, datetime.datetime)
-
-    def test_FeedDataManager_get_last_modified_returns_None_when_key_does_not_exist(self):
-        mgr = FeedManager()
-        result = mgr._get_last_modified()
-
-        self.assertIsNone(result)
-
-    def test_FeedDataManager_href_property_raises_RuntimeError_when_no_href_or_feed_object_given(self):
-        mgr = FeedManager()
-
-        with self.assertRaises(RuntimeError):
-            # noinspection PyStatementEffect
-            mgr._href
-
-    def test_FeedDataManager_href_property_returns_correct_value_when_href_given_at_init(self):
-        fake_href = 'https://example.com'
-        mgr = FeedManager(href=fake_href)
-
-        self.assertEqual(mgr._href, fake_href)
-
-    def test_FeedDataManager_href_property_returns_correct_value_when_feed_object_given_at_init(self):
-        fake_feed = FeedFactory()
-        mgr = FeedManager(feed=fake_feed)
-
-        self.assertEqual(mgr._href, fake_feed.href)
-
-    def test_FeedDataManager_href_property_returns_correct_value_when_both_href_and_feed_object_given(self):
-        fake_feed = FeedFactory()
-        fake_href = 'https://example.com'
-        mgr = FeedManager(feed=fake_feed, href=fake_href)
-
-        self.assertEqual(mgr._href, fake_href)
-
-    @mock.patch.object(feedparser, 'parse')
-    def test_FeedDataManager_entries_property_call_returns_list(self, parse):
-        parse.return_value = self.feedparserdict
-
-        mgr = FeedManager(href='https://example.com')
-        mgr.fetch_data()
-        result = mgr.entries
-
-        self.assertIsInstance(result, list)
-
-    def test_FeedDataManager_entries_property_call_returns_empty_list(self):
-        mgr = FeedManager()
-        result = mgr.entries
-
-        self.assertEqual(len(result), 0)
 
     def test_EntryDataManager_to_internal_returns_dict(self):
         feed_data = get_feedparser_parsed()
@@ -148,13 +60,3 @@ class CoreManagersTestCase(TestCase):
         result = mgr._get_published()
 
         self.assertIsNone(result)
-
-    @mock.patch.object(feedparser, 'parse')
-    def test_EntryDataManager_parse_classmethod_raise_TypeError_when_called_with_many_is_True_and_passing_in_single_item(self, parse):
-        parse.return_value = self.feedparserdict
-
-        feed_mgr = FeedManager(href='https://example.com')
-        feed_mgr.fetch_data()
-
-        with self.assertRaises(TypeError):
-            EntryDataManager.parse(feed_mgr.entries[0], many=True)
