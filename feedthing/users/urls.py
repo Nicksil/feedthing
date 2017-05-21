15e75e32d751799ec2756265b864ccda27180d27
Nick Silvester <nicksil@gmail.com>
2017-05-21 00:58:52 -0400

    Redo login

--- feedthing/users/urls.py
+++ feedthing/users/urls.py
@@ -1,8 +1,24 @@
 from django.conf.urls import url
-from django.contrib.auth.views import LoginView
+from django.contrib import messages
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.contrib.auth.views import logout
 from django.shortcuts import redirect
+from django.shortcuts import render
 from django.urls import reverse_lazy
+
+
+def login_view(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(reverse_lazy('feeds:index'))
+        messages.info(request, 'Username, password mismatch.')
+        return redirect(reverse_lazy('users:login'))
+    return render(request, 'users/login.html')
 
 
 def logout_view(request):
@@ -13,7 +29,7 @@ def logout_view(request):
 urlpatterns = [
     url(
         r'^login/$',
-        LoginView.as_view(template_name='users/login.html'),
+        login_view,
         name='login'
     ),
     url(
